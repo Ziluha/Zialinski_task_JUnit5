5eb6ae59df359bb41799feda36b7784ef7a95fb2
ICX\ivan.zialinski <ivan.zialinski@gmail.com>
2017-12-15 17:23:46 +0300

    Added: DriverSingleton

--- src/main/java/com/browser/Factory.java
+++
@@ -1,4 +0,0 @@
-package com.browser;
-
-public class Factory {
-}
---
+++ src/main/java/com/driver/DriverSingleton.java
@@ -0,0 +1,38 @@
+package com.driver;
+
+import com.enums.Browsers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSingleton {
+    private static DriverSingleton instance;
+    private WebDriver driver;
+
+    private DriverSingleton(){}
+
+    public static synchronized DriverSingleton getInstance(){
+        if(instance == null)
+            instance = new DriverSingleton();
+        return instance;
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    public void initBrowser(Browsers.name browser){
+        switch (browser){
+            case Chrome:
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+            case Firefox:
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+                driver = new FirefoxDriver();
+        }
+    }
+
+    public void CloseAllDrivers(){
+        driver.quit();
+    }
+}
